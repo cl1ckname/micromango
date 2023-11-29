6d13873734ab335b902debaa59a41230c2ea8c85
Ivan Vasilev <20002mc@gmail.com>
2023-11-29 19:02:43 +0300

    implement profile service

--- pkg/services/profile/db.go
+++ pkg/services/profile/db.go
@@ -1,6 +1,7 @@
 package profile
 
 import (
+	"github.com/google/uuid"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
@@ -30,4 +31,20 @@ func Connect(addr string) *gorm.DB {
 		log.Fatal(err)
 	}
 	return db
+}
+
+func CreateProfile(db *gorm.DB, p Profile) (Profile, error) {
+	err := db.Create(&p).Error
+	return p, err
+}
+
+func SaveProfile(db *gorm.DB, p Profile) (Profile, error) {
+	err := db.Save(&p).Error
+	return p, err
+}
+
+func FindOne(db *gorm.DB, userId uuid.UUID) (p Profile, err error) {
+	p.UserId = userId
+	err = db.First(&p).Error
+	return
 }
--- pkg/services/profile/service.go
+++ pkg/services/profile/service.go
@@ -2,8 +2,11 @@ package profile
 
 import (
 	"context"
+	"github.com/google/uuid"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
 	"gorm.io/gorm"
 	"log"
 	"micromango/pkg/common"
@@ -40,4 +43,50 @@ type service struct {
 	pb.UnimplementedProfileServer
 	db     *gorm.DB
 	static static.StaticClient
+}
+
+func (s *service) CreateProfile(_ context.Context, req *pb.CreateProfileRequest) (*pb.ProfileResponse, error) {
+	userUuid, err := uuid.Parse(req.UserId)
+	if err != nil {
+		return nil, err
+	}
+	profile := Profile{
+		UserId:   userUuid,
+		Username: req.Username,
+	}
+	newProfile, err := CreateProfile(s.db, profile)
+	if err != nil {
+		return nil, err
+	}
+	return newProfile.ToResponse(), nil
+}
+
+func (s *service) UpdateProfile(ctx context.Context, req *pb.UpdateProfileRequest) (*pb.ProfileResponse, error) {
+	userUuid, err := uuid.Parse(req.UserId)
+	if err != nil {
+		return nil, err
+	}
+	userToUpdate, err := FindOne(s.db, userUuid)
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, status.Error(codes.NotFound, "user "+req.UserId+" not found")
+		}
+		return nil, err
+	}
+
+	if len(req.Picture) != 0 {
+		uploadRes, err := s.static.UploadProfilePicture(ctx, &static.UploadProfilePictureRequest{
+			UserId: req.UserId,
+			Image:  req.Picture,
+		})
+		if err != nil {
+			return nil, err
+		}
+		userToUpdate.Picture = uploadRes.ImageId
+	}
+	updatedProfile, err := SaveProfile(s.db, userToUpdate)
+	if err != nil {
+		return nil, err
+	}
+	return updatedProfile.ToResponse(), nil
 }
