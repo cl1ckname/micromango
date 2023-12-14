6ee8c90195a7523625fddf7b0d5973fb38a2f093
Ivan Vasilev <20002mc@gmail.com>
2023-12-14 10:56:38 +0300

    update activity main file

--- cmd/activity/main.go
+++ cmd/activity/main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"context"
+	"github.com/joho/godotenv"
+	"log"
+	"micromango/pkg/services/activity"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
+
+	config := activity.Config{
+		Addr:   os.Getenv("ACTIVITY_ADDR"),
+		DbAddr: os.Getenv("ACTIVITY_DB_ADDR"),
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	ok := activity.Run(ctx, config)
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
+	select {
+	case err := <-ok:
+		log.Fatal("Server stopped: ", err)
+	case s := <-sigCh:
+		log.Printf("got signal %v, attempting graceful shutdown", s)
+		cancel()
+	}
+	log.Println("Graceful shutdown")
 }
